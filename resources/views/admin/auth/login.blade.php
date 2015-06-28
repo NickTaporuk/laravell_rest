7147ed83f51817f8e0a1bd301578418d49f65c27
NickTaporuk <nictaporuk@yandex.ru>
2015-06-28 19:49:49 +0300

    fix

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -66,11 +66,11 @@
             </div>
         </form>
 
-        <div class="social-auth-links text-center">
+        {{--<div class="social-auth-links text-center">
             <p>- OR -</p>
             <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using Facebook</a>
             <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign up using Google+</a>
-        </div>
+        </div>--}}
 
         <a href="login.html" class="text-center">I already have a membership</a>
     </div><!-- /.form-box -->
