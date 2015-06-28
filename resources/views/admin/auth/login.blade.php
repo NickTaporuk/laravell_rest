f10029c7b323741b5cb896160001fa72d956733c
NickTaporuk <nictaporuk@yandex.ru>
2015-06-28 20:08:52 +0300

    fix

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -30,36 +30,47 @@
 <body class="register-page">
 <div class="register-box">
     <div class="register-logo">
-        <a href="../../index2.html"><b>Admin</b>Slavpeople</a>
+        <a href="/"><b>Admin</b>  Slavpeople</a>
     </div>
 
     <div class="register-box-body">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <p class="login-box-msg">Register a new membership</p>
-        <form action="../../index.html" method="post">
+        <form method="POST" action="{{ url('/auth/register') }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="form-group has-feedback">
-                <input type="text" class="form-control" placeholder="Full name"/>
+                <input type="text" class="form-control" name="name" placeholder="Name" value="{{ old('name') }}"/>
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" placeholder="Email"/>
+                <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}"/>
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password"/>
+                <input type="password" class="form-control" placeholder="Password" name="password"/>
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Retype password"/>
+                <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation"/>
                 <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
             </div>
             <div class="row">
-                <div class="col-xs-8">
+                {{--<div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
                             <input type="checkbox"> I agree to the <a href="#">terms</a>
                         </label>
                     </div>
-                </div><!-- /.col -->
+                </div>--}}<!-- /.col -->
                 <div class="col-xs-4">
                     <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
                 </div><!-- /.col -->
